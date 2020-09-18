011fdfa24edb64411e007585a760a2a49b493977
esteban.moll@free.fr <esteban.moll@free.fr>
2020-09-18 14:10:15 +0200

    Premiere version avec bug

---
+++ src/neur/Main.java
@@ -0,0 +1,54 @@
+package neur;
+
+import java.util.Vector;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vector<Double> w = new Vector<Double>();
+		w.add(0.5);
+		w.add(0.0);
+		w.add(0.0);
+		
+		Vector<Integer> X = new Vector<Integer>();
+		X.add(1);
+		X.add(2);
+		X.add(0);
+		
+		Vector<Integer> Y = new Vector<Integer>();
+		Y.set(0, 1);
+		Y.set(1, 0);
+		Y.set(2, 3);
+		
+		Vector<Integer> Z = new Vector<Integer>();
+		Y.set(0, 1);
+		Y.set(1, 3);
+		Y.set(2, 0);
+		
+		Vector<Integer> J = new Vector<Integer>();
+		J.set(0, 1);
+		J.set(1, 1);
+		J.set(2, 1);
+		
+		
+		Vector<Vector<Integer>> x = new Vector<Vector<Integer>>();
+		x.add(X);
+		x.add(Y);
+		x.add(Z);
+		x.add(J);
+		
+		Vector<Integer> y = new Vector<Integer>();
+		y.add(1);
+		y.add(0);
+		y.add(0);
+		y.add(1);
+
+		Perceptron p = new Perceptron(w,x,y,0.1);
+		
+		p.Percept();
+		
+	}
+
+}
---
+++ src/neur/Perceptron.java
@@ -0,0 +1,95 @@
+package neur;
+
+import java.util.Vector;
+
+public class Perceptron {
+	
+	Vector<Double> w = new Vector<Double>();
+	Vector<Vector<Integer>> x = new Vector<Vector<Integer>>(); 
+	Vector<Integer> y = new Vector<Integer>();
+	
+	//biais
+	double alpha = 0.1f;
+	
+	Perceptron(){
+	}
+	
+	Perceptron(Vector<Double> w1, Vector<Vector<Integer>> x1, Vector<Integer> y1, double a){
+		
+		this.w = (Vector<Double>) w1.clone();
+		this.x = (Vector<Vector<Integer>>)x1.clone();
+		this.y = (Vector<Integer>)y1.clone();
+		this.alpha = a;
+		
+	}
+	
+	private int Threshold(int j) {
+		
+		double res = 0;
+		
+		for(int i = 0; i < this.w.size(); i++) {
+			
+			res += this.w.get(i) * this.x.get(j).get(i);
+			
+		}
+		
+		if (res >=0) {
+			
+			return 1;
+			
+		}
+		else {
+			
+			return 0;
+		}
+
+	}
+	
+	private void ajust(int j, int hx) {
+		
+		for(int i = 0; i < this.w.size(); i++){
+			
+			double v = this.w.get(i) + this.alpha * (this.y.get(j) - hx) * this.x.get(j).get(i);
+			
+			this.w.set(i, v);
+
+		}
+		
+	}
+	
+	public void Percept() {
+		
+		boolean change = true;
+		int j = 0;
+		int h;
+		
+		while(change && j < 100) {
+			change = false;
+			for(int i = 0; i < this.x.size(); i++) {
+				
+				h = this.Threshold(i);
+				
+				if (h != this.y.get(i)){
+					
+					this.ajust(i, h);
+					change = true;
+					
+				}
+			}
+			j++;
+		}
+		
+		if(change == false) {
+			
+			System.out.println("Le vecteur final est w : " + w);
+			
+		}
+		else {
+			
+			System.out.println("Il y a trop d'iteration");
+			
+		}
+		
+	}
+
+}
